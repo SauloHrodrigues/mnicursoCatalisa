c4d265ae23bb349b90cdee909bca0f483baeecc1
Saulo <saulohrodrigues@gmail.com>
2022-01-14 17:46:47 -0300

    consumir API

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const imagem = document.querySelector('img');
-const botao = document.querySelector('button');
+const botao = document.querySelector('#botao');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
-const status = document.querySelector('#status');
+const condicao = document.querySelector('#status');
+
+pegarPersonagem = () => { // função de chamada de API
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+        method:'GET',
+        headers: {
+            Accept: 'application/json',
+            "content-type": 'aplication/pson'
+        }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        condicao.innerHTML = data.status;
+    }); // promessa de resposta
+
+    botao.onclick = pegarPersonagem;
+
+}
+
