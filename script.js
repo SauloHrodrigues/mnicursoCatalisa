bbe94c61431e827bccc700b06d8bfee4c41291d6
Saulo <saulohrodrigues@gmail.com>
2022-01-16 11:37:12 -0300

    corrigir JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const imagem = document.querySelector('img');
+
+const imagem = document.querySelector('#imagem');
 const botao = document.querySelector('#botao');
 const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
@@ -10,20 +11,18 @@ gerarValorAleatorio = () =>{
 
 pegarPersonagem = () => { // função de chamada de API
     let  numeroAleatorio;
-    return fetch(`https://rickandmortyapi.com/api/character/${numeroAleatorio}`, {
+    return fetch(`https://rickandmortyapi.com/api/character/3`, {
         method:'GET',
         headers: {
             Accept: 'application/json',
-            "content-type": 'aplication/pson'
+            "Content-type": 'application/json'
         }
     }).then((response) => response.json()).then((data) => {
         imagem.src = data.image;
         imagem.alt = data.name;
         nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
         condicao.innerHTML = data.status;
     }); // promessa de resposta
-
-    botao.onclick = pegarPersonagem;
-
 }
-
+botao.onclick = pegarPersonagem;
